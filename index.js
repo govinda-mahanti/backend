0b68e48cc4553d435d5c16d0a9d48a574def346f
govinda-mahanti <gmahanti955@gmail.com>
2024-07-19 01:00:34 +0530

    add

--- index.js
+++ index.js
@@ -4,12 +4,37 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const githubdata=  {
+    "login": "mojombo",
+    "id": 1,
+    "node_id": "MDQ6VXNlcjE=",
+    "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/mojombo",
+    "html_url": "https://github.com/mojombo",
+    "followers_url": "https://api.github.com/users/mojombo/followers",
+    "following_url": "https://api.github.com/users/mojombo/following{/other_user}",
+    "gists_url": "https://api.github.com/users/mojombo/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/mojombo/subscriptions",
+    "organizations_url": "https://api.github.com/users/mojombo/orgs",
+    "repos_url": "https://api.github.com/users/mojombo/repos",
+    "events_url": "https://api.github.com/users/mojombo/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/mojombo/received_events",
+    "type": "User",
+    "site_admin": false
+  }
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/twitter',(req, res)=>{
     res.send("govinda")
+})
+
+app.get('/github',(req, res)=>{
+    res.json(githubdata)
 })
 
 app.get('/login',(req, res)=>{
@@ -23,3 +48,5 @@ app.get('/me',(req, res)=>{
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+//https://backend-0y5j.onrender.com
